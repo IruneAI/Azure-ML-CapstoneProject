54daff6cc208ff81b125cf9c4b158d354eb7afe1
IruneAI <irunelansorena@gmail.com>
2021-04-19 18:26:08 +0200

    updating train script and hyperdrive

--- starter_file/train.py
+++ starter_file/train.py
@@ -8,70 +8,37 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
-from azureml.data.dataset_factory import TabularDatasetFactory
+from azureml.core import Workspace, Dataset
 
 def prepare_data(data):
     #dealing with missing values
     #checking missing
-    if (data.isnull().sum()!=0):
-        x_df = data.to_pandas_dataframe().dropna()
+    #if (data.isnull().sum()!=0):
+    #    x_df = data.to_pandas_dataframe().dropna()
     #looking into feature types and distribution
     print(data.dtypes)
     #all of them are numerical--> not encoding needed.
     #leave target as it is
-    return (x_df, data['target']) 
+    return (data.loc[:,data.columns!='target'], data['target'])
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-#creating a tabularDataset
-ds =TabularDatasetFactory.from_delimited_files('https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv')### YOUR CODE HERE ###
-#cleaning data
-x, y = clean_data(ds) 
-
-# TODO: Split data into train and test sets.
-#spliting data --> 80-20
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-
+#getting run context
 run = Run.get_context()
+print('hsres')
+print (run.id)
 
-
-
-def main():
-    # Add arguments to script
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
-
-    args = parser.parse_args()
-
-    run.log("Regularization Strength:", np.float(args.C))
-    run.log("Max iterations:", np.int(args.max_iter))
-
-    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
-
-    accuracy = model.score(x_test, y_test)
-    run.log("Accuracy", np.float(accuracy))
-    os.makedirs('outputs', exist_ok=True)
-    #saving model
-    joblib.dump(model, 'outputs/model.joblib')
-    parser = argparse.ArgumentParser()
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-#creating a tabularDataset
-ds =TabularDatasetFactory.from_delimited_files('https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv')### YOUR CODE HERE ###
-#cleaning data
+#getting training data
+ws = run.experiment.workspace
+dataset = Dataset.get_by_name(ws, name='heart_disease')
+ds = dataset.to_pandas_dataframe()
+print(ds)
+#preparing data --> there is not too much preparation needed due to the nature of the data itself. Nevertheles, having a modular code for this processing task is recommendable,
 x, y = prepare_data(ds) 
 
 # TODO: Split data into train and test sets.
 #spliting data --> 80-20
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-run = Run.get_context()
+
 
 
 
@@ -100,3 +67,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+
